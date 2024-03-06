32c7926eff35691a24d091ee48b37224b6e504ee
lcfr <me@lcfr.io>
2024-03-05 23:54:31 -0500

    more

--- test/foundry/wrapper/NameWrapper.sol
+++ test/foundry/wrapper/NameWrapper.sol
@@ -5,14 +5,20 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
 import {ENSRegistry} from "contracts/registry/ENSRegistry.sol";
+import {BaseRegistrarImplementation} from "contracts/ethregistrar/BaseRegistrarImplementation.sol";
 
 contract NameWrapperTest is Test {
 
     receive() external payable {}
 
-    ENSRegistry registry;
+    ENSRegistry ens;
+    BaseRegistrarImplementation baseRegistrar;
 
     function setUp() public {
-	registry = new ENSRegistry();
+	ens = new ENSRegistry();
+    baseRegistrar = new BaseRegistrarImplementation(ens, 0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae);
     }
+
+
+
 }
